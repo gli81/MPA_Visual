7345e2b87040ec815c5f1318fe3df87d9c412986
LGZ <liguangzheng998@hotmail.com>
2024-01-29 01:07:58 -0500

    worked

--- Rshinyapp2.R
+++ Rshinyapp2.R
@@ -18,7 +18,7 @@ library(shiny)
 #--- histogram plot and a table to display the data. ----
 
 ui <- fluidPage(
-  titlePanel("Measures between 2015 and 2021"),  # Page title
+  titlePanel("MPA Index ??"),  # Page title
   sidebarLayout(
     sidebarPanel(
       # checkbox inputs for MPA 
@@ -32,11 +32,26 @@ ui <- fluidPage(
       ),
       
       # radio button inputs for index
+      #checkboxGroupInput(
+      #  inputId="select_index",
+      #  label="Select Index",
+      #  choices=c("Material Index", "Marine Tenure Index", "Food Security Index", "Place Attachment Index", "School Enrollment Rate"),
+      #  selected=NULL,
+      #  inline=FALSE
+      #),
       radioButtons(
-        inputId="select_index",
-        label="select index",
-        choices=c("AvgMAIndex", "AvgMTIndex", "AvgFSIndex", "AvgPAIndex", "AvgSERate"),
-        selected="AvgMAIndex",
+        inputId = "select_index",
+        label="Select Index",
+        choices=c("Material Index", "Marine Tenure Index", "Food Security Index", "Place Attachment Index", "School Enrollment Rate"),
+        selected="Material Index",
+        inline=FALSE
+      ),
+      
+      checkboxGroupInput(
+        choices=unique(settlement_data$TimePoint),
+        inputId="select_Time",
+        label="Select Time Point",
+        selected=NULL,
         inline=FALSE
       ),
       
@@ -58,25 +73,27 @@ ui <- fluidPage(
 #---In summary, the plot_epicurve function takes data, parent areas, and indicators as input, 
 #--- and generates an epicurve plot using the ggplot2 package ----
 
-plot_epicurve <- function(data, MPA = NULL, index = NULL) {
+plot_epicurve <- function(data, MPA=NULL, index=NULL, time=NULL) {
   
-  # Check if parentarea or indicators are NULL
-  if (is.null(MPA) || is.null(index)) {
+  ## Check if MPA or index are NULL
+  if (is.null(MPA) || is.null(index) || is.null(time)) {
     # Return empty plot with no data if either are NULL
     return(ggplot() + theme_void())
   }
   
-  # Filter data based on selected parentarea and indicators, and calculate 
-  #summary statistics (confidence intervals)
-  selected_index = substr(index, start=4, stop=nchar(index))
+  ## Filter data based on selected parentarea and indicators, and calculate 
+  ## summary statistics (confidence intervals)
+  # selected_index = substr(index, start=4, stop=nchar(index))
+  # print(index)
   sub_data <- data %>%
-    select(MPAName, TimePoint, !!sym(selected_index)) %>%
+    select(MPAName, TimePoint, !!sym(index)) %>%
     group_by(MPAName, TimePoint) %>% # group by MPA, TimePoint
-    summarize(AvgIndex = mean(!!sym(selected_index), na.rm=T), ct=n())#,
+    summarize(AvgIndex = mean(!!sym(index), na.rm=T)) %>%#,
             #  sd_measure_norm = sd(measure_norm),
              # n = n(),
               #se = sd_measure_norm / sqrt(n)) %>%
-  #  ungroup()# %>%
+    ungroup() %>%
+    filter(TimePoint %in% time)
   #mutate(lower_ci = mean_measure_norm - 1.96 * se,
   #upper_ci = mean_measure_norm + 1.96 * se)
   #print(sub_data)
@@ -101,20 +118,28 @@ plot_epicurve <- function(data, MPA = NULL, index = NULL) {
   #  mutate(lower_ci = mean_measure_norm - 1.96 * se,
   #         upper_ci = mean_measure_norm + 1.96 * se)
   
-  #print(merged_data)
-  print(sub_data)
+  # print(merged_data)
+  # print(sub_data)
   
   
   # Create bar plot with error bars and facet_wrap by parent_area
   p <- ggplot(sub_data, aes(x = MPAName, y = AvgIndex, fill = TimePoint)) +
-    geom_bar(stat = "identity", position = "dodge") +
+    geom_bar(stat = "identity", position = position_dodge(width=0.8), width=0.8) +
     #geom_errorbar(aes(ymin = lower_ci, ymax = upper_ci), width = 0.2, position = position_dodge(0.5)) +
-    scale_fill_manual(values = c("#a2dab3", "#52b3c6", "#337dbb", "#253596")) +
-    labs(title = "Measures between 2015 and 2021",
-         x = "MPA",
-         y = index,
-         fill = "Indicator") +
-    theme_bw()# +
+    scale_fill_manual(values = c(
+      "Baseline"="#a2dab3",
+      "2 Year Post"="#52b3c6",
+      "4 Year Post"="#337dbb",
+      "7 Year Post"="#253596"
+    )) +
+    labs(
+      title = paste("Change of", index, "in BHS MPA"),
+      x = '',
+      y = index,
+      fill = "Time Point"
+    ) +
+    theme_bw() + 
+    theme(axis.text.x=element_text(angle=30, vjust=0.8, hjust=0.8))# +
     #facet_wrap(~ parent_area, scales = "free_y", ncol = 1)
   
   return(p)  # Return plot
@@ -129,13 +154,22 @@ plot_epicurve <- function(data, MPA = NULL, index = NULL) {
 server <- function(input, output, session) {
   
   # Define reactive data that filters settlement_data based on user input
-  sub_data2 <- reactive({
+  filtered_data <- reactive({
     
-    if (!is.null(input$select_MPA) && !is.null(input$select_index)){
+    if (!is.null(input$select_MPA) && !is.null(input$select_index) && !is.null(input$select_Time)){
       choose_MPA <- settlement_data %>%
+        mutate(
+          `Material Index` = MAIndex,
+          `Marine Tenure Index` = MTIndex,
+          `Food Security Index` = FSIndex,
+          `Place Attachment Index` = PAIndex,
+          `School Enrollment Rate` = SERate,
+          Treatment = if_else(Treatment == 1, "MPA", "Non-MPA"),
+          SettlementID = as.integer(round(SettlementID)),
+          TimePoint = factor(TimePoint, levels=c("Baseline", "2 Year Post", "4 Year Post", "7 Year Post"))
+        ) %>%
         filter(MPAName %in% input$select_MPA) %>%
-        select(-c("MPAID")) %>%
-        mutate(Treatment=if_else(Treatment==1, "MPA", "Non-MPA"))
+        select(SettlementID, SettlementName, MPAName, Treatment, TimePoint, input$select_index)
       return(choose_MPA)
     } else {
       return(NULL)
@@ -145,25 +179,18 @@ server <- function(input, output, session) {
   
   # Render the epicurve plot using the filtered data
   output$measures_histogram <- renderPlot(
-    plot_epicurve(sub_data2(), input$select_MPA, input$select_index))
+    plot_epicurve(filtered_data(), input$select_MPA, input$select_index, input$select_Time))
   
   # Render a table showing the filtered data
   output$table_data <- renderTable({
-    if (is.null(input$select_MPA) && is.null(input$select_index)) {
+    if (is.null(input$select_MPA) || is.null(input$select_index) || is.null(input$select_Time)) {
       return(NULL)
-    } else if (is.null(sub_data2())) {
+    } else if (is.null(filtered_data())) {
       return(NULL)
     }else {
       modified_data <- reactive(
         {
-          modify <- sub_data2() %>%
-            mutate(
-              `Material Index` = MAIndex,
-              `Marine Tenure Index` = MTIndex,
-              `Food Security Index` = FSIndex,
-              `School Enrollment Rate` = SERate
-            ) %>%
-            select(-c("MAIndex", "MTIndex", "FSIndex", "SERate", "PAIndex"))
+          modify <- filtered_data()
           return(modify)
         }
       )
@@ -175,10 +202,10 @@ server <- function(input, output, session) {
   output$dwd_data <- downloadHandler(
     # Set the filename for the downloaded file
     filename = function(){
-      paste("sub_data2", input$select_parentarea, input$select_indicators, input$dtype, sep = ".")},
+      paste("filtered_data", input$select_parentarea, input$select_indicators, input$dtype, sep = ".")},
     # Write the filtered data to a file
     content = function(file){
-      write.table(sub_data2(), file=file, sep=",", row.names = F)})
+      write.table(filtered_data(), file=file, sep=",", row.names = F)})
 }
 
 #---It calls the shinyApp function, passing the UI (user interface) and server functions as arguments, 
